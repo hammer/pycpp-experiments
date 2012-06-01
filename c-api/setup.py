8ed34dcbe249d99414ceb2b11cb6f7a98c6b717c
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2012-05-31 19:24:52 -0700

    Clean up deleted files

--- c-api/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-spam = Extension('spam',
-                 sources = ['spammodule.c'])
-
-setup (name = 'spam',
-       version = '1.0',
-       description = 'spam',
-       ext_modules = [spam])
